bf05f5ea01d924cab6a3f266d82606239bcbb3b0
conana28 <33334673+conana28@users.noreply.github.com>
2023-01-01 09:23:10 +1300

    pre page four masonry

--- src/pages/PageFour.js
+++ src/pages/PageFour.js
@@ -3,38 +3,48 @@ import { Helmet } from 'react-helmet-async';
 // @mui
 import { Container, Typography, Box, Paper, styled } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
+import { Masonry } from '@mui/lab';
 // components
+import { Block } from '../sections/Block';
 import { BottleSearchDisplay } from '../components/winetrak/BottleSearchDisplay';
 import BottleSearchInput from '../components/winetrak/BottleSearchInput';
 import { useSettingsContext } from '../components/settings';
-
 // ----------------------------------------------------------------------
 
+const StyledListContainer = styled(Paper)(({ theme }) => ({
+  width: '100%',
+  border: `solid 1px ${theme.palette.divider}`,
+}));
+
 export default function PageFour() {
-  const { themeStretch } = useSettingsContext();
+  // const { themeStretch } = useSettingsContext();
+  const { ...a } = useSettingsContext();
   const [searchData, setSearchData] = useState({
     bottleSearchString: '',
     vintageSearchString: 0,
   });
 
+  console.log(a);
+
   return (
     <>
       <Helmet>
-        <title> WT | Consume a bottle</title>
+        <title>Cellar | Masonry Test</title>
       </Helmet>
 
-      <Container maxWidth={themeStretch ? false : 'xl'}>
-        <Typography variant="h3" component="h3" paragraph>
-          Consume Bottle
-        </Typography>
-
-        {searchData.bottleSearchString === '' && (
-          <BottleSearchInput setSearchData={setSearchData} />
-        )}
-
-        {searchData.bottleSearchString !== '' && (
-          <BottleSearchDisplay searchData={searchData} setSearchData={setSearchData} />
-        )}
+      <Container>
+        <Masonry columns={{ xs: 1, md: 2 }} spacing={3}>
+          <Block title="Search">
+            {/* <StyledListContainer> */}
+            <p>aaaaaaaaaaaaaaa</p>
+            {/* </StyledListContainer> */}
+          </Block>
+          <Block title="Search results">
+            <StyledListContainer>
+              <p>bbbbbbb</p>
+            </StyledListContainer>
+          </Block>
+        </Masonry>
       </Container>
     </>
   );
